bd3e1a71489af6f00e7cbe50b8ff8c3867805983
Luis Marquez <luis901101@gmail.com>
2025-02-08 00:01:39 +0000

    - Implemented tests for Places API (New) getDetails

--- test/places_new_test.dart
+++ test/places_new_test.dart
@@ -18,14 +18,118 @@ Future<void> main() async {
     test('Get Place Details with all fields', () async {
       var response = await placesAPI.getDetails(
         id: placeId ?? '',
-        fields: [],
-        // fields: ['*'],
+        allFields: true,
       );
       expect(response.isSuccessful, true);
       expect(response.body, isNotNull);
       expect(response.body?.id, placeId);
       expect(response.body?.name, isNotNull);
       expect(response.body?.location, isNotNull);
+    });
+    test('Get Place Details with specific fields list', () async {
+      var response = await placesAPI.getDetails(
+        id: placeId ?? '',
+        fields: [
+          'name',
+          'id',
+          'types',
+          'formattedAddress',
+          'addressComponents.longText',
+          'viewport.low.latitude',
+          'rating',
+          'regularOpeningHours.periods.open.day',
+        ],
+      );
+      expect(response.isSuccessful, true);
+      expect(response.body, isNotNull);
+      expect(response.body?.id, placeId);
+      expect(response.body?.name, isNotNull);
+      expect(response.body?.types, isNotNull);
+      expect(response.body?.formattedAddress, isNotNull);
+      expect(response.body?.addressComponents, isNotEmpty);
+      expect(response.body?.addressComponents?.firstOrNull, isNotNull);
+      expect(response.body?.rating, isNotNull);
+      expect(response.body?.viewport, isNotNull);
+      expect(response.body?.viewport?.low, isNotNull);
+      expect(response.body?.viewport?.low?.latitude, isNotNull);
+      expect(response.body?.regularOpeningHours, isNotNull);
+      expect(response.body?.regularOpeningHours?.periods, isNotEmpty);
+      expect(response.body?.regularOpeningHours?.periods?.firstOrNull?.open,
+          isNotNull);
+      expect(
+          response.body?.regularOpeningHours?.periods?.firstOrNull?.open?.day,
+          isNotNull);
+    });
+    test('Get Place Details with placeFields instance', () async {
+      var response = await placesAPI.getDetails(
+        id: placeId ?? '',
+        placeFields: PlaceDetails(
+          id: '',
+          name: '',
+          types: [],
+          formattedAddress: '',
+          addressComponents: [
+            AddressComponents(
+              longText: '',
+            ),
+          ],
+          rating: 0.0,
+          viewport: Viewport(
+            low: Low(
+              latitude: 0.0,
+            ),
+          ),
+          regularOpeningHours: RegularOpeningHours(
+            periods: [
+              Periods(
+                open: Open(
+                  day: 0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+      expect(response.isSuccessful, true);
+      expect(response.body, isNotNull);
+      expect(response.body?.id, placeId);
+      expect(response.body?.name, isNotNull);
+      expect(response.body?.types, isNotNull);
+      expect(response.body?.formattedAddress, isNotNull);
+      expect(response.body?.addressComponents, isNotEmpty);
+      expect(response.body?.addressComponents?.firstOrNull, isNotNull);
+      expect(response.body?.rating, isNotNull);
+      expect(response.body?.viewport, isNotNull);
+      expect(response.body?.viewport?.low, isNotNull);
+      expect(response.body?.viewport?.low?.latitude, isNotNull);
+      expect(response.body?.regularOpeningHours, isNotNull);
+      expect(response.body?.regularOpeningHours?.periods, isNotEmpty);
+      expect(response.body?.regularOpeningHours?.periods?.firstOrNull?.open,
+          isNotNull);
+      expect(
+          response.body?.regularOpeningHours?.periods?.firstOrNull?.open?.day,
+          isNotNull);
+    });
+    test('Get Place Details without fields', () async {
+      var response = await placesAPI.getDetails(
+        id: placeId ?? '',
+        fields: [],
+      );
+      expect(response.isSuccessful, false);
+      expect(response.error, isNotNull);
+      expect(response.error?.error?.code, 400);
+      expect(response.error?.error?.status, 'INVALID_ARGUMENT');
+    });
+    test('Get Place Details with invalid fields', () async {
+      var response = await placesAPI.getDetails(
+        id: placeId ?? '',
+        fields: ['test'],
+      );
+      expect(response.isSuccessful, false);
+      expect(response.error, isNotNull);
+      expect(response.error?.error?.code, 400);
+      expect(response.error?.error?.status, 'INVALID_ARGUMENT');
+      expect(response.error?.error?.details, isNotNull);
     });
   });
 }
